157be65796721b3544ef1129f873daf6ebb3a11e
Martha Nisa <nashy.nisa@gmail.com>
2025-02-20 12:09:18 +0300

    Initial Commit

---
+++ Crypto Ranking App/Crypto_Ranking_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_Ranking_AppApp.swift
+//  Crypto Ranking App
+//
+//  Created by Martha Nashipae on 20/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_Ranking_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
